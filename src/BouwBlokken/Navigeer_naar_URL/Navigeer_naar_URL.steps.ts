2e418703667594461e2dea72f8b0e5cb91db033a
anonymous012 <grf1986@users.noreply.github.com>
2025-04-19 21:19:58 +0200

    delete unwanted imports

--- src/BouwBlokken/Navigeer_naar_URL/Navigeer_naar_URL.steps.ts
+++ src/BouwBlokken/Navigeer_naar_URL/Navigeer_naar_URL.steps.ts
@@ -1,12 +1,7 @@
 import { page } from "../../hooks/hooks";
 import { Then, setDefaultTimeout } from "@cucumber/cucumber";
 import { ILogObj, Logger } from "tslog";
-import FunctionAppIndex from "../../pageObjects/TASapp/TASapp.index";
-import TASapp from "../../pageObjects/TASapp/TASapp.index";
 import { GaNaarAPP } from "../../functions/Navigeer/gaNaarApplicatie";
-
-
-
 
 
 setDefaultTimeout(60 * 100000 * 2);
