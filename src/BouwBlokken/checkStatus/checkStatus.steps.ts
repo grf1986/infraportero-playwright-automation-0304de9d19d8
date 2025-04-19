3eed341a4eb5681be17792b8142e2ee4408090eb
anonymous012 <grf1986@users.noreply.github.com>
2025-04-19 21:25:58 +0200

    to contain to toEqual

--- src/BouwBlokken/checkStatus/checkStatus.steps.ts
+++ src/BouwBlokken/checkStatus/checkStatus.steps.ts
@@ -14,14 +14,14 @@ const log: Logger<ILogObj> = new Logger();
 When('Status geupdate naar: {string} voor index: {string} op tab: {string}', async (value: string,index:number, tab: number) => {
         log.silly('Status is geupdate naar: '+ value)
         const tasApp = new TASapp(page)
-        expect(await tasApp.checkTextPage.CheckTextFunctionOnPage(index,tab)).toContain(value);
+        expect(await tasApp.checkTextPage.CheckTextFunctionOnPage(index,tab)).toEqual(value);
         log.silly("Status geupdate naar: " + value);
 });
 
 When('Gebruiker is ingelogd met naam: {string} voor index: {string} op tab: {string}', async (value: string,index:number, tab: number) => {
         log.silly('Status is geupdate naar: '+ value)
         const tasApp = new TASapp(page)
-        expect(await tasApp.checkTextPage.AccountGebruikersNaam(index,tab)).toContain(value);
+        expect(await tasApp.checkTextPage.AccountGebruikersNaam(index,tab)).toEqual(value);
         log.silly("Gebruiker is ingelogd met email: " + value);
 });
 
--- src/tests/features/demo.feature
+++ src/tests/features/demo.feature
@@ -8,5 +8,5 @@ Feature: Login
     Then Gebruiker is ingelogd met naam: "<naam>" voor index: "0" op tab: "0"
 
     Examples:
-      | Scenario | url                        | headerButton | headerValue | gebruikersnaam                   | wachtwoord      | naam                     |
-      | DemoTest | https://www.kirpalani.com/ | Account      | N.V.T       | info@testautomation-solutions.nl | Ma2!PrGP@XS8bZf | testAutomation Solutions |
+      | Scenario | url                        | headerButton | headerValue | gebruikersnaam                   | wachtwoord      | naam                    |
+      | DemoTest | https://www.kirpalani.com/ | Account      | N.V.T       | info@testautomation-solutions.nl | Ma2!PrGP@XS8bZf | testAutomation Solution |
